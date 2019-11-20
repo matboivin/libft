f674f7c8e107a41632cf005403ba78339402c02c
matboivin <mboivin@student.42.fr>
2019-11-20 19:52:55 +0100

    Add ft_lstclear

--- Makefile
+++ Makefile
@@ -96,6 +96,7 @@ SRC_FILES 	+=		ft_lstnew.c \
 					ft_lstlast.c \
 					ft_lstadd_back.c \
 					ft_lstdelone.c \
+					ft_lstclear.c \
 					ft_lstiter.c \
 					ft_lstmap.c \
 
---
+++ src/ft_lstclear.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/20 19:46:51 by mboivin           #+#    #+#             */
+/*   Updated: 2019/11/20 19:52:24 by mboivin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** function: Deletes and frees the given element and every successor of that
+** element, using the function del and free(3)
+** Finally, the pointer to the list must be set to NULL
+**
+** lst: The adress of a pointer to a element
+** new: The adress of the function used to delete the content of the element
+*/
+
+void		ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*cursor;
+	t_list	*next;
+
+	if (lst && del)
+	{
+		cursor = *lst;
+		while (cursor)
+		{
+			next = cursor->next;
+			del(cursor->content);
+			free(cursor);
+			cursor = next;
+		}
+		*lst = NULL;
+	}
+}
