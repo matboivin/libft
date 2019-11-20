cb299da17dd791eac83950f6ccad3f542e23c58b
matboivin <mboivin@student.42.fr>
2019-11-20 20:04:20 +0100

    [wip] New ft_lstmap

--- src/ft_lstmap.c
+++ src/ft_lstmap.c
@@ -6,17 +6,29 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/22 16:38:13 by mboivin           #+#    #+#             */
-/*   Updated: 2019/06/26 21:11:36 by mboivin          ###   ########.fr       */
+/*   Updated: 2019/11/20 20:03:18 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-** This function iterates a list lst and applies the function f to each link
-** to create a “fresh” list (using malloc(3)) resulting from the successive
-** applications of f. If the allocation fails, the function returns NULL.
+** function: Iterates the list lst and applies the function f to the content of
+** each element
+** Creates a new list resulting of the successive applications of the function f
+** The del function is here to delete the content of an element if needed
+**
+** lst: The address of a pointer to a element
+** f: The address of the function to iterate on the list
+**
+** returns: The new list
+**          NULL otherwise
 */
+
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+
+}
 
 t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
