f25fecdab9fa7485de169b1c2d62fc3443013e5a
Mathilde Boivin <mboivin@student.42.fr>
2019-12-02 22:26:25 +0100

    Correct variable name

--- src/ft_strdup.c
+++ src/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 17:23:32 by mboivin           #+#    #+#             */
-/*   Updated: 2019/11/20 18:12:09 by mboivin          ###   ########.fr       */
+/*   Updated: 2019/12/02 22:25:51 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,20 @@
 ** Recoded strdup libc function: Allocates sufficient memory for a copy of the
 ** string s1, does the copy, and returns a pointer to it
 **
-** src: The string to be copied
+** s1: The string to be copied
 **
 ** returns: A pointer to the copy
 **          NULL otherwise
 */
 
-char		*ft_strdup(char const *src)
+char		*ft_strdup(char const *s1)
 {
 	size_t	len;
 	char	*s;
 
-	len = ft_strlen(src);
+	len = ft_strlen(s1);
 	if (!(s = (char *)malloc(sizeof(*s) * len + 1)))
 		return (NULL);
-	s = ft_strcpy(s, src);
+	s = ft_strcpy(s, s1);
 	return (s);
 }
