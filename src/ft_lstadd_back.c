f529278d8042d6785221633452a152ca7f324d93
matboivin <mboivin@student.42.fr>
2019-12-10 15:08:35 +0100

    Fix ft_lstadd_back

--- src/ft_lstadd_back.c
+++ src/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/20 19:41:33 by mboivin           #+#    #+#             */
-/*   Updated: 2019/12/04 23:46:30 by mboivin          ###   ########.fr       */
+/*   Updated: 2019/12/10 15:08:08 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,14 @@ void		ft_lstadd_back(t_list **lst, t_list *new)
 	t_list	*cursor;
 
 	if (!lst || !new)
- 		return ;
+		return ;
 	cursor = *lst;
-	while (cursor->next)
-		cursor = cursor->next;
-	cursor = new;
+	if (lst && *lst)
+	{
+		while (cursor->next)
+			cursor = cursor->next;
+		cursor->next = new;
+	}
+	else
+		*lst = new;
 }
