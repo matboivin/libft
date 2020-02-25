015c8dfa09051b41caf8e2b9a704466e4d03869e
matboivin <mboivin@student.42.fr>
2020-02-25 18:47:18 +0100

    Fix wrong name

--- includes/libft_includes.h
+++ includes/libft_includes.h
@@ -6,12 +6,12 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/09 09:30:53 by mboivin           #+#    #+#             */
-/*   Updated: 2020/02/25 15:57:01 by mboivin          ###   ########.fr       */
+/*   Updated: 2020/02/25 17:56:01 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_INCLUDE_H
-# define LIBFT_INCLUDE_H
+#ifndef LIBFT_INCLUDES_H
+# define LIBFT_INCLUDES_H
 
 # include <unistd.h>
 # include <stdlib.h>
