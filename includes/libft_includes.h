3e8dc2d921da431b0f926292d5bf9d36515671a8
motya <mboivin@student.42.fr>
2020-05-29 18:39:53 +0200

    Add stdint.h

--- includes/libft_includes.h
+++ includes/libft_includes.h
@@ -6,7 +6,7 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/09 09:30:53 by mboivin           #+#    #+#             */
-/*   Updated: 2020/05/01 14:38:59 by mboivin          ###   ########.fr       */
+/*   Updated: 2020/05/29 18:35:00 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <limits.h>
+# include <stdint.h>
 
 #endif
